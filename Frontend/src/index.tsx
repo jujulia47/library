e263c5d65986cd62709a231d3c5942427e6c41cc
Júlia Borges <julia.borges@corebiz.ag>
2024-02-09 17:04:49 -0300

    adjust and remove code

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/css/index.css';
+// import './styles/css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AppProvider } from './context/index'; 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <AppProvider >
-      <App/>
-    </AppProvider>
+    <App/>
   </React.StrictMode>
 );
 
